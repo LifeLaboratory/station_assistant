a10f542e6d55e571e3c2ed7af8ade748ba071266
raldenprog <raldenprog@mail.ru>
2019-05-19 02:42:28 +0700

    set point fix

--- backend/app/route/route_set_point.py
+++ backend/app/route/route_set_point.py
@@ -1,17 +1,17 @@
 from app.api.base import base_name as names
 from app.api.base.base_router import BaseRouter
 from app.api.src.touches_from_api import add_new_point
+from app.config.config import HEADER
 
 
 class RouteSetPoint(BaseRouter):
 
     def __init__(self):
         super().__init__()
-        self.args = self.args = [names.ORIGIN_X, names.ORIGIN_Y, names.TYPE, names.TIME, names.DESCRIPTION
-            , names.DATETIME, names.TIME, names.RATING]
+        self.args = self.args = [names.ORIGIN_X, names.ORIGIN_Y, names.TYPE, names.TIME, names.DESCRIPTION,
+                                 names.DATETIME, names.TIME, names.RATING]
 
-
-    def set_point(self):
-
+    def get(self):
         self._read_args()
         add_new_point(self.data)
+        return 'OK', HEADER
